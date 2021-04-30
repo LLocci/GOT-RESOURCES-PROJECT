0175312737c0836753b23b07dddd9f7d52d8102f
LLocci <lou.locci@gmail.com>
2021-04-30 16:38:05 +0200

    Add images for the books

--- src/app/books/book-list.component.ts
+++ src/app/books/book-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { IBook } from './book';
 import { Subscription } from 'rxjs';
 import { BookService } from './books.service';
@@ -8,21 +8,37 @@ import { BookService } from './books.service';
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.css']
 })
-export class BookListComponent implements OnInit {
+export class BookListComponent implements OnInit, OnDestroy {
 
     showImage : boolean = false; 
     listFilter : string = 'book';
     sub!: Subscription;
+    imageWidth = 60;
+    imageMargin = 5;
+    errorMessage = '';
     books : IBook[] = [] ;
 
   constructor(private bookService: BookService) {}
+
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
+
+  getImagePath(title : string) : string {
+      return '../assets/images/'+title.replace(/ /g, "_").toLowerCase()+'.jpg';
+  }
 
   ngOnInit(): void {
     this.sub = this.bookService.getBooksFromServer().subscribe({
         next: books => {
           this.books = books;
-        }
+        },
+        error : err => this.errorMessage = err
     })
  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
 
 }

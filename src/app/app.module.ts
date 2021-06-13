c562e02cbe8efa397ea79e95433064eadcfb8d56
LLocci <lou.locci@gmail.com>
2021-06-13 13:41:54 +0200

    Added exapansion panel to book details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { BookDetailsComponent } from './books/book-details/book-details.component';
 
 
@@ -35,7 +36,8 @@ import { BookDetailsComponent } from './books/book-details/book-details.componen
     MatCardModule,
     MatButtonModule,
     MatIconModule,
-    MatGridListModule
+    MatGridListModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
